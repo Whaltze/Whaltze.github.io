b981477af00c559ae6b36e36a6d23ae9ffc89006
Whaltze <2260274457@qq.com>
2025-03-02 13:59:45 +0800

    Site updated: 2025-03-02 13:59:44

--- js/cherry.js
+++ js/cherry.js
@@ -68,7 +68,7 @@
   }
   function Particle() {
     this.character = "*";
-    this.lifeSpan = 240; //ms
+    this.lifeSpan = 120; //ms
     this.initialStyles ={
       "position": "fixed",
       "top": "0", //必须加
